f3b54b82e44dd51d590263bee37935b596896ee7
Gaurav <gauravrawat0259@gmail.com>
2024-04-11 08:07:04 +0530

    added ques-qualify the round on codechef

---
+++ Qualify the round/ans.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,x,a,b;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>a>>b;
+	    
+	    if(a+(b*2) >= x) cout<<"Qualify"<<endl;
+	    else cout<<"NOTQUALIFY"<<endl;
+	    
+	}
+	
+}
