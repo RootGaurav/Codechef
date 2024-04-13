739a052cf39e0b3d35cfd49f118ae3818e11f437
Gaurav <gauravrawat0259@gmail.com>
2024-04-13 09:41:09 +0530

    Added Minimum cars required

---
+++ Minimum Cars required/ans.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,n;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n;
+	    if(n%4>0) cout<<n/4 + 1 <<endl;
+	    else cout<<n/4<<endl;
+	    
+	    
+	}
+	
+}
