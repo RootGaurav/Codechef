8a32a62f096a5a46319b9672551cefa38f036c51
Gaurav <gauravrawat0259@gmail.com>
2024-04-18 08:29:36 +0530

    Added Maximise the Tastiness

---
+++ Maximise the Tastiness/ans.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,a,b,c,d;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>a>>b>>c>>d;
+	    cout<<max(a,b)+max(c,d)<<endl;
+	    
+	}
+	
+}
