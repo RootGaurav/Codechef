76917e8cbfa202d86304d87a511cfcc41ebb75e1
Gaurav <gauravrawat0259@gmail.com>
2024-04-25 21:33:40 +0530

    Added Mario and Transformations

---
+++ Mario and Transformation/ans.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x%3==0)
+	    cout<<"NORMAL"<<endl;
+	    else if(x%3==1)
+	    cout<<"HUGE"<<endl;
+	    else
+	    cout<<"SMALL"<<endl;
+	    
+	}
+	return 0;
+}
