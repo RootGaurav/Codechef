49e719008029990b5fbbfc8af2050aad60a9220a
Gaurav <gauravrawat0259@gmail.com>
2024-05-01 09:06:59 +0530

    Added Finding

---
+++ Finding Square Roots/ans.cpp
@@ -0,0 +1,19 @@
+// We have populated the solutions for the 10 easiest problems for your support.
+// Click on the SUBMIT button to make a submission to this problem.
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int t,n;
+    cin>>t;
+    for (int i = 0; i < t; i++) {
+        /* code */
+        int n;
+         cin >> n;
+         cout << int(sqrt(n)) << endl;
+    }
+    
+    return 0;
+}
