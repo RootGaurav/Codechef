355ae35ac02ee26a9daae4484e6fc8d91424c236
Gaurav <gauravrawat0259@gmail.com>
2024-06-18 11:51:02 +0530

    Added Cyclic Quadrilateral

---
+++ Cyclic Quadrilateral/ans.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,a,b,c,d;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>a>>b>>c>>d;
+	    if(a+c == 180 && b+d==180) cout<<"yes"<<endl;
+	    else cout<<"no"<<endl;
+	    
+	}
+	
+}
