5a35a2aff81be1634d81d36f100f6ef36ab79664
Gaurav <gauravrawat0259@gmail.com>
2024-06-20 11:23:02 +0530

    Added Too many items

---
+++ Too many items/ans.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,n,k,x;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n;
+	    if(n%10 >= 1) cout<<(n/10)+1<<endl;
+	    else cout<<n/10<<endl;
+	    
+	}
+	
+}
