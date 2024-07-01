c0dfe92ebb592688d73db1bb6b26108db6c8d7b6
Gaurav <gauravrawat0259@gmail.com>
2024-07-01 08:27:44 +0530

    Added Dracula Eats

---
+++ Dracula Eats/ans.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    x+=5;
+	    cout<<floor(x*1.0/7)<<endl;
+	}
+	return 0;
+}
