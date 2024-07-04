97854065ba9c306180dc50d5098039fb47548a52
Gaurav <gauravrawat0259@gmail.com>
2024-07-04 10:56:49 +0530

    Added Chef

---
+++ Chef And Operators/ans.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,n,k,x;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n>>k;
+	    if(n>k)cout<<">"<<endl;
+	    else if(n<k) cout<<"<"<<endl;
+	    else
+	    cout<<"="<<endl;
+	    
+	}
+	
+}
