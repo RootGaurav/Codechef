17c90a096113862d19aa72c60030daf2b6d37abc
Gaurav <gauravrawat0259@gmail.com>
2024-07-19 12:30:00 +0530

    Added  International Justice Day

---
+++ International Justice Day/ans.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x, y;
+	cin >> x >> y;
+	
+	if(x >= y)
+	{
+	    cout << "yes" << endl;
+	}
+	else
+	{
+	    cout << "No" << endl;
+	}
+
+}
