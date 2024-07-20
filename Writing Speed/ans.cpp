958afef01795899291c5c2890d60aaafda8116f4
Gaurav <gauravrawat0259@gmail.com>
2024-07-20 20:49:42 +0530

    Added Writing Spped

---
+++ Writing Speed/ans.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int X;
+	cin >> X;
+	
+	if (X * 5 <= 60) {
+	    cout << "YES" << endl;
+	} else {
+	    cout << "NO" << endl;
+	}
+	
+	return 0;
+}
